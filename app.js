ddafff9cfa4b0022e5f496cc2016559f0fb5f1d4
Maria Viana do Nascimento <mariaviana05091997@gmail.com>
2025-02-04 22:27:03 -0300

    Implementada as funcionalidades de adicionar nomes à lista, atualizar  a lista e realizar o sorteio de forma aleatória.

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+// Função para adicionar amigos na lista
+function adicionarAmigo() {
+  const input = document.getElementById("amigo");
+  const nome = input.value.trim();
+
+  if (nome !== "" && !amigos.includes(nome)) {
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+  } else if (amigos.includes(nome)) {
+    alert("Esse nome já foi adicionado!");
+  } else {
+    alert("Digite um nome válido.");
+  }
+}
+
+// Função para atualizar a lista de amigos na tela
+function atualizarLista() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    const item = document.createElement("li");
+    item.textContent = amigo;
+    lista.appendChild(item);
+  });
+}
+
+// Função para sortear apenas um nome da lista
+function sortearAmigo() {
+  if (amigos.length < 1) {
+    alert("Adicione pelo menos um amigo para o sorteio.");
+    return;
+  }
+
+  const nomeSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>O amigo secreto sorteado é: ${nomeSorteado}</li>`;
+}
